ff1d802296f6a72a35a72270306a636da06291b2
Weipeng Zheng <dastonzerg93@gmail.com>
2019-04-19 07:28:31 -0700

    Update WordCntRun.java

--- WordCntRun.java
+++ WordCntRun.java
@@ -2,16 +2,20 @@ import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.File;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class WordCntRun implements Runnable {
-    private StringBuilder inputSb;
+    private StringBuilder inputSb=null;
     private Hashtable<String, Integer> mainTable;
-    private Map<String, Integer> wordCnt=new HashMap<>();
+    private Map<String, Integer> wordCnt=null;
+    private BufferedReader reader;
+    private ReentrantLock lock;
+    private final int chunkSize=5000_0000;
 
-
-    private WordCntRun(StringBuilder _inputSb, Hashtable<String, Integer> _mainTable) {
-        inputSb =_inputSb;
+    private WordCntRun(Hashtable<String, Integer> _mainTable, BufferedReader _reader, ReentrantLock _lock) {
         mainTable=_mainTable;
+        reader=_reader;
+        lock=_lock;
     }
 
     private static boolean isDelims(char ch) {
@@ -24,6 +28,25 @@ public class WordCntRun implements Runnable {
         return false;
     }
 
+    private void fetchSb() throws Exception {
+        inputSb=new StringBuilder();
+        wordCnt=new HashMap<>();
+        for(int i=1; i<=chunkSize; i++) {
+            int charValue=reader.read();
+            if(charValue==-1) {
+                return;
+            }
+            inputSb.append((char)charValue);
+        }
+        if(!isDelims(inputSb.charAt(inputSb.length()-1))) {
+            int charValue = reader.read();
+            while (charValue != -1 && !isDelims((char)charValue)) {
+                inputSb.append((char)charValue);
+                charValue=reader.read();
+            }
+        }
+    }
+
     private void updateToMainTable() {
         for(Map.Entry<String, Integer> entry:wordCnt.entrySet()) {
             String word=entry.getKey();
@@ -33,65 +56,63 @@ public class WordCntRun implements Runnable {
     }
 
     public void run() {
-        int start=0;
-        for(int i = 0; i< inputSb.length(); i++) {
-            if(isDelims(inputSb.charAt(i))) {
-                if(i-1>=start) {
-                    String word= inputSb.substring(start, i);
-                    wordCnt.put(word, wordCnt.getOrDefault(word, 0)+1);
-                }
-                start=i+1;
+        while(true) {
+            lock.lock();
+            try {
+                fetchSb();
+            } catch (Exception e) {
+                e.printStackTrace();
+                inputSb=null;
+                wordCnt=null;
+                break;
+            } finally {
+                lock.unlock();
             }
+            if (inputSb.length() == 0) {
+                break;
+            }
+            int start = 0;
+            for (int i = 0; i < inputSb.length(); i++) {
+                if (isDelims(inputSb.charAt(i))) {
+                    if (i - 1 >= start) {
+                        String word = inputSb.substring(start, i);
+                        wordCnt.put(word, wordCnt.getOrDefault(word, 0) + 1);
+                    }
+                    start = i + 1;
+                }
+            }
+            if (!isDelims(inputSb.charAt(inputSb.length() - 1))) {
+                String word = inputSb.substring(start);
+                wordCnt.put(word, wordCnt.getOrDefault(word, 0) + 1);
+            }
+            updateToMainTable();
+            inputSb = null;
+            wordCnt=null;
         }
-        if(!isDelims(inputSb.charAt(inputSb.length()-1))) {
-            String word= inputSb.substring(start);
-            wordCnt.put(word, wordCnt.getOrDefault(word, 0)+1);
-        }
-        updateToMainTable();
-        inputSb=null;
     }
 
     public static void main(String[] args) throws Exception {
-        if(args.length!=1) {
+        if(args.length!=2) {
             System.out.println(Runtime.getRuntime().maxMemory());
             System.out.println(Runtime.getRuntime().freeMemory());
             System.out.println("Not enough arguments. Usage: \"WordCntRun.java filepath threadNum\"");
             return;
         }
         long startTime=System.currentTimeMillis();
-     //   int N=Integer.valueOf(args[1]);
+        int N=Integer.valueOf(args[1]);
         File file=new File(args[0]);
         BufferedReader reader=new BufferedReader(new FileReader(file));
         System.out.println(file.length());
-      //  final int chunkSize=(int)(file.length()/N)+(file.length()%N==0?0:1);
-        final int chunkSize=5000_0000;
-        final int N=(int)(file.length()/chunkSize)+(file.length()%chunkSize==0?0:1);
-        System.out.println("chunkSize: "+chunkSize);
-        char[] tempReadChars=new char[chunkSize];
         List<Thread> threadList=new ArrayList<>();
         PriorityQueue<Map.Entry<String, Integer>> minHeap=new PriorityQueue<>(
                 (o1, o2)->!o1.getValue().equals(o2.getValue())
                         ?Integer.compare(o1.getValue(), o2.getValue())
                         :o2.getKey().compareTo(o1.getKey()));
         Hashtable<String, Integer> mainTable=new Hashtable<>();
+        ReentrantLock lock=new ReentrantLock();
 
         for(int i=1; i<=N; i++) {
-            int numReaded=reader.read(tempReadChars, 0, chunkSize);
-            if(numReaded==-1) {
-                break;
-            }
-            StringBuilder sb=new StringBuilder();
-            for(int j=0; j<numReaded; j++) {
-                sb.append(tempReadChars[j]);
-            }
-            if(!isDelims(tempReadChars[numReaded-1])) {
-                int charValue = reader.read();
-                while (charValue != -1 && !isDelims((char) charValue)) {
-                    sb.append((char)charValue);
-                    charValue=reader.read();
-                }
-            }
-            Thread thread=new Thread(new WordCntRun(sb, mainTable));
+            Thread thread=new Thread(new WordCntRun(mainTable, reader, lock));
             thread.start();
             threadList.add(thread);
         }
